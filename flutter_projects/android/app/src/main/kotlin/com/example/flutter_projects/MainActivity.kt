cb13782fda92d3521bf2d6c3c6181b410203b736
DarshanN29 <darshannarayan29@gmail.com>
2025-12-01 11:59:22 +0530

    Add flutter_projects as a top-level subfolder

---
+++ flutter_projects/android/app/src/main/kotlin/com/example/flutter_projects/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.example.flutter_projects
+
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+
+class MainActivity : FlutterFragmentActivity() {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+    }
+}
